80d6e9fd8ebdb1c75166579efa57a03a59edc8cf
Jeweeted <f0rex-corperations@hotmail.com>
2011-05-16 15:41:09 +0200

    GUI prototype uitgewerkt

--- PAZ/MainWindow.xaml.cs
+++ PAZ/MainWindow.xaml.cs
@@ -17,11 +17,71 @@ namespace PAZ
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+    /// 
+
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
         }
+
+        private void buttonExportPDF_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Weet u zeker dat u het rooster wilt omzetten naar een pdf bestand?", "Bevestiging", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                MessageBoxResult result = MessageBox.Show("Het PDF-bestand is gemaakt.", "PDF-bestand gemaakt");
+            }
+        }
+
+        private void buttonVerwijderGebruikers_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Weet u zeker dat u alle gebruikers wilt verwijderen? \n\nLet op: deze actie kan niet ongedaan worden.", "Bevestiging", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                MessageBoxResult result = MessageBox.Show("Succesvol. Alle gebruikers zijn verwijderd.", "Gebruikers verwijderd");
+            }
+        }
+
+        private void buttonVerwijderLokalen_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Weet u zeker dat u alle lokalen wilt verwijderen? \n\nLet op: deze actie kan niet ongedaan worden.", "Bevestiging", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+            {
+                MessageBoxResult result = MessageBox.Show("Succesvol. Alle lokalen zijn verwijderd.", "Lokalen verwijderd");
+            }
+        }
+
+        private void comboBoxSelecteerType_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (comboBoxSelecteerType.SelectedIndex > 0)
+            {
+                verbergAlleToevoegGroupBoxs();
+
+                switch (comboBoxSelecteerType.SelectedIndex)
+                {
+                    case 1: groupBoxLokaalGegevens.Visibility = Visibility.Visible; break;
+                    case 2: groupBoxGebruikerGegevens.Visibility = Visibility.Visible; break;
+                    case 3: groupBoxBegeleiderGegevens.Visibility = Visibility.Visible; break;
+                    case 4: groupBoxExternGegevens.Visibility = Visibility.Visible; break;
+                    case 5: groupBoxLeraarGegevens.Visibility = Visibility.Visible; break;
+                }
+            }
+        }
+
+        private void verbergAlleToevoegGroupBoxs()
+        {
+            groupBoxLokaalGegevens.Visibility = Visibility.Hidden;
+            groupBoxGebruikerGegevens.Visibility = Visibility.Hidden;
+            groupBoxBegeleiderGegevens.Visibility = Visibility.Hidden;
+            groupBoxExternGegevens.Visibility = Visibility.Hidden;
+            groupBoxLeraarGegevens.Visibility = Visibility.Hidden;
+        }
+
+
+
+
+
     }
 }

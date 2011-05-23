8664167bf835ce87e3f05ff5fe12c370550f9d83
ibsonic <i.onder@live.nl>
2011-05-23 13:14:13 +0200

    Meerlagenarchitectuur toegevoegd

---
+++ PAZ/View/Login.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PAZ
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+       
+        private void labelButton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MainWindow tt = new MainWindow();
+            tt.Show();
+            Application.Current.Windows[0].Close();
+            this.Close();
+        }
+
+    }
+}

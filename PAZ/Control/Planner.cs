8d60dd038bdbd199b1a1428fa4d19b0f2f078259
teuneboon <teuneboon@gmail.com>
2011-06-08 04:50:52 -0700

    Edited PAZ/Control/Planner.cs via GitHub

--- PAZ/Control/Planner.cs
+++ PAZ/Control/Planner.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using PAZ.Model;
+using PAZMySQL;
 
 namespace PAZ.Control
 {
@@ -40,9 +41,30 @@ namespace PAZ.Control
             return score;
         }
 
-        public Planning Plan()
+        public Planning Plan(List<Pair> pairs)
         {
-            return null;
+            Planning result = new Planning();
+
+            List<Classroom> classrooms = new ClassroomMapper(MysqlDb.GetInstance()).FindAll();
+            List<Daytime> daytimes = new DaytimeMapper(MysqlDb.GetInstance()).FindAll();
+            int classroomI = 0;
+            int daytimeI = 0;
+            foreach (Pair pair in pairs)
+            {
+                Session session = new Session();
+                session.Classroom = classrooms[classroomI];
+                session.Daytime = daytimes[daytimeI];
+                session.Pair = pair;
+                result.Sessions.Add(session);
+                ++classroomI;
+                if (classroomI == classrooms.Count)
+                {
+                    classroomI = 0;
+                    ++daytimeI;
+                }
+            }
+            Console.WriteLine(this.CheckScore(result));
+            return result;
         }
     }
 }

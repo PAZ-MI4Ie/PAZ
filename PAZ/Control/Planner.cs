d36471a360d3bc6eb593181e5ac2400f94d694ce
teuneboon <teuneboon@gmail.com>
2011-06-08 04:23:31 -0700

    Edited PAZ/Control/Planner.cs via GitHub

--- PAZ/Control/Planner.cs
+++ PAZ/Control/Planner.cs
@@ -1,11 +1,46 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using PAZ.Model;
 
 namespace PAZ.Control
 {
-	class Planner
-	{
-	}
+    class Planner
+    {
+        private int CheckScore(Planning planning)
+        {
+            int score = 0;
+            foreach (Session session in planning.Sessions)
+            {
+                foreach (User user in session.Pair.Participants)
+                {
+                    foreach (Blocked_timeslot blockedTimeSlot in user.BlockedTimeslots)
+                    {
+                        if (session.Daytime.Equals(blockedTimeSlot.Daytime))
+                        {
+                            if (!blockedTimeSlot.Hardblock)
+                            {
+                                ++score;
+                            }
+                            else
+                            {
+                                return -1;
+                            }
+                        }
+                        else
+                        {
+                            score += 2;
+                        }
+                    }
+                }
+            }
+            return score;
+        }
+
+        public Planning Plan()
+        {
+            return null;
+        }
+    }
 }

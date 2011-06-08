2e774c6c2bd692f05862256775ea1f83f8aa3361
teuneboon <teuneboon@gmail.com>
2011-06-08 04:44:19 -0700

    Edited PAZ/Model/Planning.cs via GitHub

--- PAZ/Model/Planning.cs
+++ PAZ/Model/Planning.cs
@@ -10,6 +10,21 @@ namespace PAZ.Model
      */
     class Planning
     {
-        public List<Session> Sessions { get; set; }
+        private List<Session> _sessions;
+        public List<Session> Sessions
+        {
+            get
+            {
+                if (this._sessions == null)
+                {
+                    this.Sessions = new List<Session>();
+                }
+                return this._sessions;
+            }
+            set
+            {
+                this._sessions = value;
+            }
+        }
     }
 }

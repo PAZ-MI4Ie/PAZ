4153e6857db82ec77b0fe58500830066808febf3
Yorg Kuijs <yorg.kuijs@home.nl>
2011-06-22 02:09:26 -0700

    Lokalen verwijderen verwijderd

--- PAZ/View/DeleteDataWindow.xaml.cs
+++ PAZ/View/DeleteDataWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,18 +12,14 @@ using PAZ.View;
 namespace PAZ
 {
     /**
-    * In deze klassen kun je de te verwijderen gegevens bepalen
-    * 
-    * Auteur: Gökhan en Yorg 
+    * In deze klasse kun je de te verwijderen gegevens bepalen
     */
     public partial class DeleteDataWindow : Window
     {
         private List<User> _users;
-        private List<Classroom> _classrooms;
         private List<object> _deleteList;
 
         private List<CheckBox> _userBoxes;
-        private List<CheckBox> _classroomBoxes;
 
         private string _selectedReceiverEmail = string.Empty;
 
@@ -36,7 +32,6 @@ namespace PAZ
             _controller = PAZController.GetInstance();
 
             _users = _controller.UserMapper.FindAll();
-            _classrooms = _controller.ClassroomMapper.FindAll();
             _deleteList = new List<object>();
 
             List<User> _admins = new List<User>();
@@ -51,17 +46,13 @@ namespace PAZ
                 _users.Remove(admin);
 
             _users.Sort();
-            _classrooms.Sort();
 
-            LokalenToevoegen();
             PersonenToevoegen();            
         }
 
 
         /**
         * Voegt personen toe en geeft weer als checkboxen
-        * 
-        * Auteur: Gökhan en Yorg
         */
         private void PersonenToevoegen()
         {
@@ -105,49 +96,9 @@ namespace PAZ
         }
 
        
-        /**
-        * Voegt lokalen toe en geeft weer als checkboxen
-        * 
-        * Auteur: Gökhan en Yorg
-        */
-        private void LokalenToevoegen()
-        {
-            _classroomBoxes = new List<CheckBox>();
-            Canvas canvasClassrooms = new Canvas();
-
-            int left = 0, top = 0;
-            double rightMost = 0;
-            foreach (Classroom classroom in _classrooms)
-            {
-                CheckBox cb = new CheckBox();
-                cb.Checked += new RoutedEventHandler(cb_Checked);
-                cb.Unchecked += new RoutedEventHandler(cb_Unchecked);
-                cb.Content = classroom.Room_number;
-                cb.Tag = classroom; // koppelt object aan checkbox
-
-                Canvas.SetLeft(cb, left);
-                Canvas.SetTop(cb, top);
-                canvasClassrooms.Children.Add(cb);
-
-                _classroomBoxes.Add(cb);
-
-                top += 20;
-                double newRightMost = left + cb.Width;
-                if (rightMost < newRightMost)
-                    rightMost = newRightMost;
-            }
-            canvasClassrooms.Width = rightMost;
-            canvasClassrooms.Height = top;
-
-            canvasClassrooms.HorizontalAlignment = HorizontalAlignment.Left;
-            canvasClassrooms.VerticalAlignment = VerticalAlignment.Top;
-
-            scrollViewerLokalen.Content = canvasClassrooms;
-        }
 
         /**
         * Haalt de student of docent uit de _deleteList en unchecked de selector
-        * Auteur: Gökhan en Yorg
         */
         private void cb_Unchecked(object sender, RoutedEventArgs e)
         {
@@ -155,15 +106,11 @@ namespace PAZ
             // verwijder de gekoppelde object van de _deleteList
             _deleteList.Remove(cb.Tag);
 
-            if (cb.Tag is User)
-                HandleSelectorUnchecking(cb, cbxPersoonSelector, _userBoxes);
-            else
-                HandleSelectorUnchecking(cb, cbxLokaalSelector, _classroomBoxes);
+            HandleSelectorUnchecking(cb, cbxPersoonSelector, _userBoxes);
         }
 
         /**
          * Hanteer het unchecken van een selector, omdat deze alles unchecked, moeten alle boxes weer gecheckt worden behalve degene die unchecked werdt
-         * Auteur: Gökhan en Yorg 
          */
         private void HandleSelectorUnchecking(CheckBox uncheckedBox, CheckBox selectorBox, List<CheckBox> checkBoxes)
         {
@@ -177,7 +124,6 @@ namespace PAZ
 
         /**
         * Voegt de geselecteerde data toe aan de _deleteList en markeert de selector als checked als alle checkboxes checked zijn.
-        * Auteur: Gökhan en Yorg
         */
         private void cb_Checked(object sender, RoutedEventArgs e)
         {
@@ -185,20 +131,13 @@ namespace PAZ
             // voeg de gekoppelde object toe aan de _deleteList lijst
             _deleteList.Add(cb.Tag);
 
-            bool isUser = cb.Tag is User;
-            if (CheckAllChecked(isUser ? _userBoxes : _classroomBoxes))
-            {
-                if (isUser)
-                    cbxPersoonSelector.IsChecked = true;
-                else
-                    cbxLokaalSelector.IsChecked = true;          
-            }
+            if (CheckAllChecked(_userBoxes))
+                cbxPersoonSelector.IsChecked = true;      
         }
 
         /**
          * Controleert of alle checkboxes gechecked zijn
          * Return: true als ze allemaal gechecked zijn
-         * Auteur: Gökhan en Yorg 
          */
         private bool CheckAllChecked(List<CheckBox> checkBoxes)
         {       
@@ -213,8 +152,6 @@ namespace PAZ
 
         /**
         * Stuurt het commando om de geselecteerde gegevens te verwijderen
-        * 
-        * Auteur: Gökhan en Yorg
         */
         private void btnBevestigen_Click(object sender, RoutedEventArgs e)
         {
@@ -248,19 +185,8 @@ namespace PAZ
             HandleUncheck(_userBoxes);
         }
 
-        private void cbxLokalenSelector_Checked(object sender, RoutedEventArgs e)
-        {
-            HandleCheck(_classroomBoxes);
-        }
-
-        private void cbxLokalenSelector_Unchecked(object sender, RoutedEventArgs e)
-        {
-            HandleUncheck(_classroomBoxes);
-        }
-
         /**
          * Hanteert het checken van alle checkboxen
-         * Auteur: Gökhan en Yorg 
          */
         private void HandleCheck(List<CheckBox> checkBoxes, CheckBox ignoreBox = null)
         {
@@ -273,7 +199,6 @@ namespace PAZ
 
         /**
          * Hanteert het unchecken van alle checkboxen
-         * Auteur: Gökhan en Yorg 
          */
         private void HandleUncheck(List<CheckBox> checkBoxes)
         {
